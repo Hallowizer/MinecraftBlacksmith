2f02648293369ea878d8d568c453eaff605d9734
Hallowizer <matthew@munge.com>
2018-11-02 17:09:54 -0700

    Add a basic transformer api.

---
+++ api/apiCoreMods.hpp
@@ -0,0 +1,14 @@
+//
+//  coreMods.hpp
+//  
+//
+//  Created by Hallowizer on 11/2/18.
+//
+//
+
+#ifndef coreMods_h
+#define coreMods_h
+
+typedef (char *)(*modTransformer)(char *name, char *bytes);
+
+#endif /* coreMods_h */
--- common/relauncher/coreMods.cpp
+++ common/relauncher/coreMods.cpp
@@ -6,12 +6,26 @@
 //
 //
 
+#include <list>
+
+#include "../../api/apiCoreMods.hpp";
 #include "coreMods.hpp"
+
+using namespace std;
+
+static list<modTransformer> transformers;
 
 void setupCoreMods(void) {
     
 }
 
+static void registerTransformer(modTransformer transformer) {
+    transformers.push_back(transformer);
+}
+
 char *transformMod(char *name, char *bytes) {
+    for (list<modTransformer>::iterator iter = transformers.begin(); iter != transformers.end(); iter++)
+        bytes = (*iter)(name, bytes);
     
+    return bytes;
 }
