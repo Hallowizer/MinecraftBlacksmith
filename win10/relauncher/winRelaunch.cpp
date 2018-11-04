410401eea93996a8a421d42a53fdf912979f46bb
Hallowizer <matthew@munge.com>
2018-11-03 23:10:45 -0700

    Begin windows 10 relaunched.

---
+++ win10/relauncher/winRelaunch.cpp
@@ -0,0 +1,13 @@
+//
+//  winRelaunch.cpp
+//  MinecraftBlacksmith
+//
+//  Created by Hallowizer on 11/3/18.
+//
+//
+
+#include "../../common/relauncher/relauncher.hpp"
+
+char *mainName(void) {
+    return "main";
+}
