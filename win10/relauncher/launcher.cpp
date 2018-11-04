18477951ea7b4cb610445e1881bc9b6984b0e13d
Hallowizer <matthew@munge.com>
2018-11-03 23:33:42 -0700

    Create windows 10 launcher.

---
+++ win10/relauncher/launcher.cpp
@@ -0,0 +1,14 @@
+//
+//  launcher.cpp
+//  MinecraftBlacksmith
+//
+//  Created by Hallowizer on 11/3/18.
+//
+//
+
+#include "../../common/relauncher/relauncher.hpp"
+
+int main(int argc, char *argv) {
+    relaunch(argc, argv);
+    return 0; // Should not reach here.
+}
