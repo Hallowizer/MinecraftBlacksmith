92970ecd27c94476a04213c22bc378a35cd6b914
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2018-12-01 21:21:20 -0800

    Newlines are important...

--- decompiler/assemblyProcessing/main.cpp
+++ decompiler/assemblyProcessing/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		cerr << "Usage: processasm <source> <destination>";
+		cerr << "Usage: processasm <source> <destination>\n";
 		return 1;
 	}
 	
